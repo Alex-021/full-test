a79ee12372616c90dfe83886e07e0247f05c32c0
Alex-021 <ali021ball@gmail.com>
2021-10-19 01:46:57 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php
-echo "Hello Dear..."."<br>";
-$user = "swbcsfjlkdpmxy";
-$pass = "230f3b3e18c1b36230767101fb25aea119911c36ba6cc2af15b15822225b1e9a";
-$host = "ec2-3-221-100-217.compute-1.amazonaws.com";
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["postgres://swbcsfjlkdpmxy:230f3b3e18c1b36230767101fb25aea119911c36ba6cc2af15b15822225b1e9a@ec2-3-221-100-217.compute-1.amazonaws.com:5432/dasaur93oo75cr
-  "]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+$dsn = "pgsql:"
+    . "host=ec2-3-221-100-217.compute-1.amazonaws.com;"
+    . "dbname=dasaur93oo75cr;"
+    . "user=swbcsfjlkdpmxy;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=230f3b3e18c1b36230767101fb25aea119911c36ba6cc2af15b15822225b1e9a";
+
+$db = new PDO($dsn);
+
+
+$query = "SELECT * FROM user_data;";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["userID"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["countMsG"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
+    echo "</tr>";
 }
-
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
-
+$result->closeCursor();
 /*
 include 'Telegram.php';
 
