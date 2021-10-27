0f0f1c03b1602f2f77ae7a6eeb4bf21cea8bbc87
Alex-021 <ali021ball@gmail.com>
2021-10-27 12:04:05 +0330

    Update index.php

--- index.php
+++ index.php
@@ -314,27 +314,26 @@ else { // Is ADMIN //
             break;
         case "کاربران":
             function getList() {
-                $query = "SELECT * FROM user_data;";
-                $result = $db->query($query);
-                $i = 1;
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    $t_id = $row["userid"];
-                    $t_fname = $row["fname"];
-                    $num = $row["number"];
-                    $colsArr[] = $telegram->buildInlineKeyBoardButton("کاربر #$num: $t_fname","", "$t_id");
-                    if ($i % 2 == 0) {
-                        $rowsArr[] = $colsArr;
-                        unset($colsArr);
-                    }
-                    $i++;
-                }
-                if ($i % 2 == 0)
-                $rowsArr[] = $colsArr;
-                $result->closeCursor();
-                return $rowsArr;
+                
             }
-
-            $option = getList();
+            $query = "SELECT * FROM user_data;";
+            $result = $db->query($query);
+            $i = 1;
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $t_id = $row["userid"];
+                $t_fname = $row["fname"];
+                $num = $row["number"];
+                $colsArr[] = $telegram->buildInlineKeyBoardButton("کاربر #$num: $t_fname","", "$t_id");
+                if ($i % 2 == 0) {
+                    $rowsArr[] = $colsArr;
+                    unset($colsArr);
+                }
+                $i++;
+            }
+            if ($i % 2 == 0)
+            $rowsArr[] = $colsArr;
+            $result->closeCursor();
+            $option = $rowsArr;
             $keyb = $telegram->buildInlineKeyBoard($option);
             $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => '
             لیست کاربران: 
