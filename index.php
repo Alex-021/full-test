1ebd908dc5d6adeca0e0b5c4398971b2e774efb7
Alex-021 <ali021ball@gmail.com>
2021-10-28 13:06:25 +0330

    Update index.php

--- index.php
+++ index.php
@@ -345,7 +345,7 @@ else { // Is ADMIN //
                     array($telegram->buildInlineKeyBoardButton("ارسال پیام", "", $callback_data = "$user"),
                           $telegram->buildInlineKeyBoardButton("قطع ارتباط", "", $callback_data = "$user")),
                     array($telegram->buildInlineKeyBoardButton("✏️ ویرایش", "", $callback_data = "$user"),
-                          $telegram->buildInlineKeyBoardButton("حذف", "", $callback_data = "حذف|123"))
+                          $telegram->buildInlineKeyBoardButton("حذف", "", $callback_data = "حذف|$user"))
                         );
                     $keyb = $telegram->buildInlineKeyBoard($option);
                 $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "
